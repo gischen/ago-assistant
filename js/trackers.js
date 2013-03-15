1c3df74ad0a7f20701479fb4d8570543d816195c
Evan Caldwell <evan.caldwell@gmail.com>
2013-03-15 17:21:28 -0400

    added github tracker

---
+++ js/trackers.js
@@ -0,0 +1,23 @@
+// Load the gaug.es tracker.
+var _gauges = _gauges || [];
+(function() {
+    var t   = document.createElement('script');
+    t.type  = 'text/javascript';
+    t.async = true;
+    t.id    = 'gauges-tracker';
+    t.setAttribute('data-site-id', '50ea501df5a1f55810000050');
+    t.src = '//secure.gaug.es/track.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(t, s);
+})();
+
+// Load the Google Analytics tracker.
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-37459343-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
